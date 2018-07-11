53a36d5a6581a2cf96454ef1655f0252103a41e5
Alex Felipe <alexfelipevieira@gmail.com>
2018-07-11 09:37:02 -0300

    ajustando o teste para que faça os asserts das instâncias

--- app/src/test/java/alexf/com/br/techstore/database/DatabaseTest.kt
+++ app/src/test/java/alexf/com/br/techstore/database/DatabaseTest.kt
@@ -1,6 +1,8 @@
 package alexf.com.br.techstore.database
 
 import android.content.Context
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
@@ -15,13 +17,22 @@ class DatabaseTest {
 
     @Test
     fun `should get the same instance of Database when run threads simultaneously`() {
+        val dbInstances = mutableListOf<AppDatabase>()
         repeat(10) {
             thread(start = true) {
                 var element = Database.instance(context)
-                println(element)
+                dbInstances.add(element)
             }
         }
         Thread.sleep(500)
+
+        if (dbInstances.isNotEmpty()) {
+            val iterator = dbInstances.iterator()
+            val first = iterator.next()
+            if (iterator.hasNext()) {
+                assertThat(first, equalTo(iterator.next()))
+            }
+        }
     }
 
 }
